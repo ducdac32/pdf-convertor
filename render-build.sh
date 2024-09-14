c8bdda25b2aee2e13bb932c21cbc862ee6995e32
ducdac32 <dcummins9029@gmail.com>
2024-09-13 23:33:27 -0400

    udpate

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# exit on errorset -o errexit
 
 npm install
-# npm run build # uncomment if required
 
 # Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
@@ -12,3 +10,4 @@ else
   echo "...Storing Puppeteer Cache in Build Cache" 
   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
 fi
+
