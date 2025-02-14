3898b09d7cab7d855abb4887a869d112c02af009
ducdac32 <150677620+ducdac32@users.noreply.github.com>
2025-02-14 11:05:42 -0500

    block scaping bot

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ const PORT = process.env.PORT || 4000;
 // Enable CORS for all routes
 app.use(cors());
 
+app.use((req, res, next) => {
+  const userAgent = req.get('User-Agent') || '';
+
+  const botKeywords = [
+    'bot',
+    'crawl',
+    'spider',
+    'slurp',
+    'bingbot',
+    'googlebot',
+    'duckduckbot',
+    'baiduspider',
+    'yandexbot',
+    'screamingfrog',
+    'semrushbot',
+    'ahrefsbot',
+    'mj12bot',
+    'facebookexternalhit',
+    'linkedinbot',
+    'twitterbot'
+  ];
+
+  if (botKeywords.some((keyword) => userAgent.toLowerCase().includes(keyword))) {
+    console.log(`Blocked bot: ${userAgent}`);
+    return res.status(403).send('Bots are not allowed');
+  }
+
+  next();
+});
+
+
 app.get("/download-pdf", async (req, res) => {
   try {
     const { url } = req.query;
